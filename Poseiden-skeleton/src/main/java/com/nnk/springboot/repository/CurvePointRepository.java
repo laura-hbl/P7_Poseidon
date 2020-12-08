e0f7b5235e2b3fee78947c2be4b20b35f0ef3542
Laura <laura_habdul@hotmail.fr>
2020-12-08 02:08:03 +0100

    Refactor repository classes with @repository annotation

---
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/repository/CurvePointRepository.java
@@ -0,0 +1,10 @@
+package com.nnk.springboot.repository;
+
+import com.nnk.springboot.domain.CurvePoint;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CurvePointRepository extends JpaRepository<CurvePoint, Integer> {
+
+}
