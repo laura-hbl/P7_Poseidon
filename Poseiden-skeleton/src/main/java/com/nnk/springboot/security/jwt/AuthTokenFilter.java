d05545f255ecbdcf81e87d8de7ffee0c1f54b51e
Laura <laura_habdul@hotmail.fr>
2020-12-22 02:32:11 +0100

    Add json web token security classes: AuthTokenFilter and JwtUtils

---
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/security/jwt/AuthTokenFilter.java
@@ -0,0 +1,58 @@
+package com.nnk.springboot.security.jwt;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class AuthTokenFilter extends OncePerRequestFilter {
+
+    private static final Logger LOGGER = LogManager.getLogger(AuthTokenFilter.class);
+
+    @Autowired
+    private JwtUtils jwtUtils;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(final HttpServletRequest request, final HttpServletResponse response,
+                                    final FilterChain filterChain)
+            throws ServletException, IOException {
+        LOGGER.debug("Inside AuthTokenFilter's doFilterInternal method");
+
+        try {
+            Cookie[] cookies = request.getCookies();
+            String jwt = cookies[0].getValue();
+
+            if (jwt != null && jwtUtils.validateJwtToken(jwt)) {
+                String username = jwtUtils.getUserNameFromJwtToken(jwt);
+
+                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities());
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        } catch (Exception e) {
+            LOGGER.error("Cannot set user authentication: {}", e);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
---
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/security/jwt/JwtUtils.java
@@ -0,0 +1,62 @@
+package com.nnk.springboot.security.jwt;
+
+import io.jsonwebtoken.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtils {
+
+    private static final Logger LOGGER = LogManager.getLogger(JwtUtils.class);
+
+    @Value("${poseidon.app.jwtSecret}")
+    private String jwtSecret;
+
+    @Value("${poseidon.app.jwtExpirationMs}")
+    private int jwtExpirationMs;
+
+    public String generateJwtToken(final Authentication authentication) {
+        LOGGER.debug("Inside JwtUtils's generateJwtToken method");
+
+        UserDetails userPrincipal = (UserDetails) authentication.getPrincipal();
+
+        return Jwts.builder()
+                .setSubject((userPrincipal.getUsername()))
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
+                .signWith(SignatureAlgorithm.HS512, jwtSecret).compact();
+    }
+
+    public String getUserNameFromJwtToken(final String token) {
+        LOGGER.debug("Inside JwtUtils's getUserNameFromJwtToken method");
+
+        return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public boolean validateJwtToken(final String authToken) {
+        LOGGER.debug("Inside JwtUtils's validateJwtToken method");
+
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken);
+            return true;
+        } catch (SignatureException e) {
+            LOGGER.error("Invalid JWT signature: {}", e.getMessage());
+        } catch (MalformedJwtException e) {
+            LOGGER.error("Invalid JWT token: {}", e.getMessage());
+        } catch (ExpiredJwtException e) {
+            LOGGER.error("JWT token is expired: {}", e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            LOGGER.error("JWT token is unsupported: {}", e.getMessage());
+        } catch (IllegalArgumentException e) {
+            LOGGER.error("JWT claims string is empty: {}", e.getMessage());
+        }
+
+        return false;
+    }
+}
